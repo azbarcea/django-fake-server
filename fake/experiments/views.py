5b37195bf6c8ca513edaa268a2e30fb6282858d7
Alexandru Zbarcea <zbarcea.a@gmail.com>
2013-06-14 14:36:24 +0300

    fix json serialization for tests list

--- fake/experiments/views.py
+++ fake/experiments/views.py
@@ -1,7 +1,7 @@
 # Create your views here.
 
 from django.http import HttpResponse
-from django.core import serializers
+from django.utils import simplejson
 
 from experiments.models import Test
 
@@ -9,5 +9,16 @@ def index(request):
   return HttpResponse("Hello Chenil")
 
 def tests(request):
-    output = serializers.serialize("json", Test.objects.all())
-    return HttpResponse(output, mimetype="application/json")
+  output = simplejson.dumps( { 'tests': [{
+    'description': o.description, 
+    'location': o.location,
+    'prod_left': o.prod_left,
+    'prod_right': o.prod_right,
+    'created_date': o.created_date.__str__(),
+    'start_date': o.start_date.__str__(),
+    'stop_date': o.stop_date.__str__(),
+    'weight_left': str(o.weight_left) + ' g',
+    'weight_right': str(o.weight_right) + ' g',
+    'history': o.history,
+  } for o in Test.objects.all()]}, "tests" )
+  return HttpResponse(output, mimetype="application/json")
